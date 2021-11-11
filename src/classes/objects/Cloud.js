2dd0287049480099212944c8585b4e8644d6e86e
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-11-11 15:00:54 -0800

    clouds fall

--- src/classes/objects/Cloud.js
+++ src/classes/objects/Cloud.js
@@ -1,6 +1,8 @@
 import UpdatedScene from "../template/scenes/UpdatedScene.js";
 
 export default class Cloud extends Phaser.Physics.Arcade.Sprite {
+	strength = 20;
+
 	/**
 	 * Creates an instance of Cloud.
 	 * @param {UpdatedScene} scene
@@ -14,11 +16,27 @@ export default class Cloud extends Phaser.Physics.Arcade.Sprite {
 		scene.add.existing(this);
 		scene.physics.add.existing(this);
 
-		scene.physics.add.collider(scene.entities, this);
+		scene.physics.add.collider(scene.entities, this, this.fall);
 
 		this.setImmovable(true).setOffset(0, 17).body.setAllowGravity(false);
 		this.body.checkCollision.down = false;
 		this.body.checkCollision.left = false;
 		this.body.checkCollision.right = false;
 	}
+
+	fall = (entity) => {
+		if (this.body.touching.up) {
+			this.strength -= 1;
+			setTimeout(() => {
+				if (!this.body.touching.up) this.strength = 20;
+			}, 100);
+			if (this.strength === 0) {
+				this.body.checkCollision.none = true;
+				setTimeout(() => {
+					this.strength = 20;
+					this.body.checkCollision.none = false;
+				}, 1000);
+			}
+		}
+	};
 }
