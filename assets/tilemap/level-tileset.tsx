6341313fb9037a6c276370ed1b4cc295f0226e52
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-11-14 15:02:54 -0800

    change .aseprite file names

--- assets/tilemap/level-tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Level" tilewidth="16" tileheight="16" tilecount="88" columns="8">
- <image source="../sprites/tileset/tileset.png" width="128" height="176"/>
-</tileset>
